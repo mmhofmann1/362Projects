ded3af7d80e27889d884ad6fc674fea9859583e6
Matthew Hofmann <mhofmann@hawk.iit.edu>
2019-02-05 17:07:43 -0600

    Configure submit button to work

--- p1/site.js
+++ p1/site.js
@@ -1,5 +1,18 @@
 'use strict';
 //listener for submit button
-document.getElementById('submit').addEventListener("click", function() {
-  
-})
+var n=document.getElementById("name");
+var e=document.getElementById("email");
+document.getElementById("signup").addEventListener("submit", function(event) {
+if(n.validity.valid && e.validity.valid){
+  event.preventDefault();
+  incomplete.style.display = "none";
+  complete.style.display = "block";
+}});
+//editing invalid or empty input message
+var em=document.getElementById("email");
+em.addEventListener("input", function(event){
+  if(em.validity.typeMismatch){
+    em.setCustomValidity("Please enter a valid email")}
+  else{
+    em.setCustomValidity("");
+  }});
